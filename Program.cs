15dd39d24dbe5844cbcdbcdf9f4da171be420c82
leticialbuk <leticia.albuk@gmail.com>
2023-07-03 17:34:47 -0300

    fazendo UPDATE com entity

--- Program.cs
+++ Program.cs
@@ -9,9 +9,19 @@ namespace Blog
         {
             using(var context = new BlogDataContext()) 
             {
-                var tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
-                context.Tags.Add(tag);  
-                context.SaveChanges();  
+                //Create
+                //var tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+                //context.Tags.Add(tag);  
+                //context.SaveChanges();
+
+                //Update
+                var tag = context.Tags.FirstOrDefault(x=>x.Id == 3);
+                tag.Name = ".NET";
+                tag.Slug = "dotnet";
+
+                context.Update(tag);
+                context.SaveChanges();
+                
             }
         }
     }
